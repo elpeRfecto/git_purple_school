cb1b3777fde4fb326d2b50e015b8216704122047
Alexey Nekrasov <mmjkee@yandex.ru>
2025-07-22 20:53:06 +0300

    created functions in file.go and storage.go

--- 3-struct/bins/bins.go
+++ 3-struct/bins/bins.go
@@ -6,23 +6,18 @@ import (
 
 type Bin struct {
 	CreatedAt time.Time
-	Id string
-	Name string
-	private bool
+	Id        string
+	Name      string
+	private   bool
 }
 
-var BinList[]Bin
+var BinList []Bin
 
-func main() {
-	
-}
-
-
-func newBin(id, name string, private bool, createdAt time.Time) *Bin {
+func NewBin(id, name string, private bool, createdAt time.Time) *Bin {
 	return &Bin{
-		Id: id, 
-		Name: name, 
-		CreatedAt: createdAt,
-		private: private, 
+		Id:        id,
+		Name:      name,
+		CreatedAt: time.Now(),
+		private:   private,
 	}
 }
--- 3-struct/file/file.go
+++ 3-struct/file/file.go
@@ -1,1 +1,22 @@
 package file
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ReadFile(filename string) {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Ошибка чтения файла", err)
+	}
+	fmt.Printf("Содержимое файла: %s", file)
+}
+
+func IsJSON(filename string) bool {
+	if strings.Contains(filename, ".json") {
+		return true
+	}
+	return false
+}
